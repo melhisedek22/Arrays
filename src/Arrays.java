1b1fb19d924272bd23f2e8949a1d571d2b383f48
Nikolay Nilov <melhisedek22@gmail.com>
2023-10-18 23:51:29 +0300

    New project

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] a1 = new int[10];
+        int[] a2 = {34, 2, 1, 878};
+        int[] a3 = {5, 0};
+
+        System.out.println(a1.length);
+        System.out.println(a2.length);
+        System.out.println(a3.length);
+    }
+}
