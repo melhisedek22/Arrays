95d227e14503003f79c63b855a692de99d688fa7
Nikolay Nilov <melhisedek22@gmail.com>
2023-10-19 00:14:17 +0300

    New project

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,7 +2,7 @@ public class Arrays {
     public static void main(String[] args) {
         int[] a1 = new int[10];
         int[] a2 = {34, 2, 1, 878};
-        int[] a3 = {5, 0};
+        int[] a3 = {5, 0, 34};
 
         System.out.println(a1.length);
         System.out.println(a2.length);
